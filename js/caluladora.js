a949c0787ba07e584c640876c59f31ce7ee95ef9
Rodolfo <rqm@outlook.es>
2024-09-18 14:39:57 -0600

    Inicio validacion

--- js/caluladora.js
+++ js/caluladora.js
@@ -13,31 +13,36 @@ let resultado;
 
 function logica() {
 
-    switch (Number(select1.value)) {
-        case 12:
-            resultado = Number(numeUno.value) * 1.20;
-            numTres.value = 20;
-            res(resultado)
-            break;
-        case 24:
-            resultado = Number(numeUno.value) * 1.25;
-            numTres.value = 25;
-            console.log(resultado);
-            break;
-        case 36:
-            resultado = Number(numeUno.value) * 1.30;
-            numTres.value = 30;
-            console.log(resultado);
-            break;
-        default:
-            resultado = Number(numeUno.value) * 1.35;
-            numTres.value = 35;
-            console.log(resultado);
-            break;
+
+    if (numeUno.value === '' || !radioUno.checked && !radioDos.checked) {
+        
+        errores();
+
+    } else {
+        switch (Number(select1.value)) {
+            case 12:
+                resultado = Number(numeUno.value) * 1.20;
+                numTres.value = 20;
+                res(resultado)
+                break;
+            case 24:
+                resultado = Number(numeUno.value) * 1.25;
+                numTres.value = 25;
+                console.log(resultado);
+                break;
+            case 36:
+                resultado = Number(numeUno.value) * 1.30;
+                numTres.value = 30;
+                console.log(resultado);
+                break;
+            default:
+                resultado = Number(numeUno.value) * 1.35;
+                numTres.value = 35;
+                console.log(resultado);
+                break;
+        }
     }
-
 }
-
 
 function res(res) {
     if (radioUno.checked) {
@@ -56,3 +61,8 @@ function limpiar() {
 }
 
 /* Fata validaciones */ 
+
+
+function errores() {
+    console.log("no valida");
+}
